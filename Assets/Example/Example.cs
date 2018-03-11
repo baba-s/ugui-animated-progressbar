e22bea67e46f8b7f9ee98e84e76131a90872223b
baba_s <shotababa1206@gmail.com>
2018-03-11 10:57:53 +0900

    初コミット

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+	[SerializeField] private AnimatedProgressbar	m_progressbarUI	= null	;
+	[SerializeField] private float					m_speed			= 0		;
+
+	private float m_fillAmount;
+
+	private IEnumerator Start()
+	{
+		while ( m_fillAmount < 1 )
+		{
+			m_fillAmount += m_speed;
+			m_progressbarUI.FillAmount = m_fillAmount;
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/AnimatedProgressbar.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class AnimatedProgressbar : MonoBehaviour
+{
+	[SerializeField] private RawImage	m_barUI			= null	;
+	[SerializeField] private float		m_speed			= 0		;
+	[SerializeField] private float		m_fillAmount	= 0		;
+
+	private float	m_initUvRectWidth	;
+	private bool	m_isInit			;
+
+	public float FillAmount
+	{
+		get { return m_fillAmount; }
+		set
+		{
+			Init();
+
+			m_fillAmount = Mathf.Clamp01( value );
+
+			var localScale = m_barUI.rectTransform.localScale;
+			localScale.x = m_fillAmount;
+			m_barUI.rectTransform.localScale = localScale;
+
+			var rect = m_barUI.uvRect;
+			rect.width = m_initUvRectWidth * m_fillAmount;
+			m_barUI.uvRect = rect;
+		}
+	}
+
+	private void Init()
+	{
+		if ( m_isInit ) return;
+		m_isInit = true;
+
+		m_initUvRectWidth = m_barUI.uvRect.width;
+
+		FillAmount = m_fillAmount;
+	}
+
+	private void Awake()
+	{
+		Init();
+	}
+
+	private void Update()
+	{
+		var rect = m_barUI.uvRect;
+		rect.x -= m_speed;
+		m_barUI.uvRect = rect;
+	}
+}
